d1343c3871dda58b30ee0a16e32a5003c1cd1dca
Mahesh776 <maheshsanger3@gmail.com>
2026-03-03 16:53:32 +0530

    Add Navigation component

---
+++ components/Navigation.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <nav>
+            <h1>EduMaster Pro</h1>
+            <ul>
+                <li>Home</li>
+                <li>Classes</li>
+                <li>Quizzes</li>
+                <li>Doubt Solver</li>
+                <li>Leaderboard</li>
+                <li>Analytics</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
